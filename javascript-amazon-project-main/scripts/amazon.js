70d91a5e5d5d102aa2224fb83052a6e9ddc0b503
Ajilalitha <ajilalitha010@gmail.com>
2024-12-10 19:25:07 +0530

    add the cart [age

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -41,19 +41,47 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart ">
             <img src="images/icons/checkmark.png">
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
      `;
-
- console.log(productsHTML);
+});
+//  console.log(productsHTML);
  
  document.querySelector('.js-products-grid').innerHTML = productsHTML;
+ innerHTML = productsHTML;
 
-});
+ document.querySelectorAll('.js-add-to-cart')
+   .forEach((button)=> {
+    button.addEventListener('click', ()=> {
+        const productName = button.dataset.productName;
+
+        let matchingItem;
+
+        cart.forEach((item) => {
+           if (productName === item.productName) {
+             matchingItem = item;
+           }
+        });
+
+        if (matchingItem) {
+          matchingItem.quantity += 1;
+        } else {
+          cart.push({
+            productName: productName,
+            quantity: 1
+          });
+        }
+       
+        console.log(cart);
+        
+    })
+   });
+
+
